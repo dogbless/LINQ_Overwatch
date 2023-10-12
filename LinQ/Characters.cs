c8a15aec97effb71427b26d892c26434a3f0399d
Jesse Lustig <136506002+dogbless@users.noreply.github.com>
2023-10-12 18:20:15 +0200

    Add files via upload

---
+++ LinQ/Characters.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp38
+{
+	public class Characters 
+	{
+		private string heroName;
+		private string heroType;
+		private int heroHp;
+		private List<string> HeroAbilities;
+
+		public string HeroName
+		{
+			get { return heroName; }
+			set { heroName = value; }
+		}
+
+		
+
+		public string HeroType
+		{
+			get { return heroType; }
+			set { heroType = value; }
+		}
+		
+
+		public int HeroHp
+		{
+			get { return heroHp; }
+			set { heroHp = value; }
+		}
+
+		
+
+		public List<string> heroAbilities
+		{
+			get { return HeroAbilities; }
+			set { HeroAbilities = value; }
+		}
+
+		public static void Display(List<Characters> characters)
+		{
+			foreach (var item in characters)
+			{
+                if (item.HeroName == "Junkrat")
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine("Hero name: " + item.heroName);
+                    Console.WriteLine("Hero type: " + item.heroType);
+                    Console.WriteLine("Hero Health: " + item.heroHp);
+                    Console.WriteLine("----------------------------");
+
+
+                }
+                else if (item.HeroName == "Lucio")
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Hero name: " + item.heroName);
+                    Console.WriteLine("Hero type: " + item.heroType);
+                    Console.WriteLine("Hero Health: " + item.heroHp);
+                    Console.WriteLine("----------------------------");
+
+                }
+                else if (item.HeroName == "Roadhog")
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Hero name: " + item.heroName);
+                    Console.WriteLine("Hero type: " + item.heroType);
+                    Console.WriteLine("Hero Health: " + item.heroHp);
+                    Console.WriteLine("----------------------------");
+
+                }
+                else if (item.HeroName == "Mercy")
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Hero name: " + item.heroName);
+                    Console.WriteLine("Hero type: " + item.heroType);
+                    Console.WriteLine("Hero Health: " + item.heroHp);
+                    Console.WriteLine("----------------------------");
+
+                }
+                else if (item.HeroName == "Reinhardt")
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Hero name: " + item.heroName);
+                    Console.WriteLine("Hero type: " + item.heroType);
+                    Console.WriteLine("Hero Health: " + item.heroHp);
+                    Console.WriteLine("----------------------------");
+
+                }
+                else if (item.HeroName == "Bastion")
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    Console.WriteLine("Hero name: " + item.heroName);
+                    Console.WriteLine("Hero type: " + item.heroType);
+                    Console.WriteLine("Hero Health: " + item.heroHp);
+                    Console.WriteLine("----------------------------");
+                }
+                else if (item.HeroName == "Torbjorn")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Hero name: " + item.heroName);
+                    Console.WriteLine("Hero type: " + item.heroType);
+                    Console.WriteLine("Hero Health: " + item.heroHp);
+                    Console.WriteLine("----------------------------");
+
+                }
+                else if (item.HeroName == "Widow maker")
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("Hero name: " + item.heroName);
+                    Console.WriteLine("Hero type: " + item.heroType);
+                    Console.WriteLine("Hero Health: " + item.heroHp);
+                    Console.WriteLine("----------------------------");
+
+                }
+                else if (item.HeroName == "Tracer")
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("Hero name: " + item.heroName);
+                    Console.WriteLine("Hero type: " + item.heroType);
+                    Console.WriteLine("Hero Health: " + item.heroHp);
+                    Console.WriteLine("----------------------------");
+
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+
+
+
+
+
+
+
+            }
+		}
+
+
+
+
+
+
+
+
+
+
+	}
+}
---
+++ LinQ/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp38
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var weakHeroes = new List<Characters>();
+            var AlottaHealht = new List<Characters>();
+            var filterList = new List<Characters>();
+            var listOfHeroes = new List<Characters>
+            
+            {
+                 new Characters { HeroName="Junkrat", HeroType="Dps",HeroHp=200, heroAbilities=new List<string>{"mine","riptire","trap"} },
+                 new Characters { HeroName="Roadhog", HeroType="Tank",HeroHp=600, heroAbilities=new List<string>{"Hook","Heal","Whole-hog"} },
+                 new Characters { HeroName="Mercy", HeroType="Support",HeroHp=200, heroAbilities=new List<string>{"Heal Boost","Glide","Revive","Fly"} },
+                 new Characters { HeroName="Reinhardt", HeroType="Tank",HeroHp=500, heroAbilities=new List<string>{"Shield","Hammer","Firestrike"} },
+                 new Characters { HeroName="Bastion", HeroType="Dps",HeroHp=300, heroAbilities=new List<string>{"Turret transformation","Grenade","Tank"} },
+                 new Characters { HeroName="Torbjorn", HeroType="Dps",HeroHp=200, heroAbilities=new List<string>{"Turret","Bolt gun","Molten Core"} },
+                 new Characters { HeroName="Widow maker", HeroType="Dps",HeroHp=175, heroAbilities=new List<string>{"Sniper","Venom mine","Grapple Hook"} },
+                 new Characters { HeroName="Tracer", HeroType="Dps",HeroHp=150, heroAbilities=new List<string>{"Rewind","Blink","PulseBomb"} },
+                 new Characters { HeroName="Lucio", HeroType="Support",HeroHp=200, heroAbilities=new List<string>{"Heal","Speed","Boop","Wall ride"} }
+
+
+            } ;
+            Characters.Display(listOfHeroes);
+
+            filterList = listOfHeroes.Where(e => e.HeroType == "Dps" ).ToList();
+            Console.WriteLine("\n(Dps Heroes)");
+            foreach (var item in filterList)
+            {
+
+                Console.WriteLine( item.HeroName);
+            }
+            Console.WriteLine("\n");
+            AlottaHealht = listOfHeroes.Where(n => n.HeroHp > 200).ToList();
+            Console.WriteLine("(Characters with health over 200)");
+            foreach (var item2 in AlottaHealht)
+            {
+                Console.WriteLine(item2.HeroName + ": Health = " + item2.HeroHp);
+
+
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("(Characters with health under 200)");
+            weakHeroes = listOfHeroes.Where(l => l.HeroHp < 200).ToList();
+            foreach (var item in weakHeroes)
+            {
+                Console.WriteLine($"{item.HeroName}: Health = {item.HeroHp}");
+            }
+
+            
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
